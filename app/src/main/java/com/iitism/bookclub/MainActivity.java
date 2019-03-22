7d5216cc48f95e8e5eef402db775d762c207542c
rishabh-997 <rishabh.agarwal997@gmail.com>
2019-03-22 23:04:01 +0530

    Added Scanner in the app

--- app/src/main/java/com/iitism/bookclub/MainActivity.java
+++ app/src/main/java/com/iitism/bookclub/MainActivity.java
@@ -1,13 +1,25 @@
 package com.iitism.bookclub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b=findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,BarCodeScanner.class));
+            }
+        });
     }
 }
