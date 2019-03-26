db290ff0fe34816bc79f79e0c98fcb92e35602f6
Apoorva Raj Bhadani <apoorva222g@gmail.com>
2019-03-26 23:47:26 +0530

    firebase integrated

--- app/src/main/java/com/iitism/bookclub/MainActivity.java
+++ app/src/main/java/com/iitism/bookclub/MainActivity.java
@@ -3,7 +3,7 @@ package com.iitism.bookclub;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class  MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
