22fc7113e3e1a89d696f30f9fbae52e349588d1b
uditDasari <uditdasari19@gmail.com>
2019-03-28 22:28:20 +0530

    SignInActivity Createdgit status

--- app/src/main/java/com/iitism/bookclub/MainActivity.java
+++ app/src/main/java/com/iitism/bookclub/MainActivity.java
@@ -1,13 +1,23 @@
 package com.iitism.bookclub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class  MainActivity extends AppCompatActivity {
-
+Button SignInbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SignInbutton = findViewById(R.id.button);
+        SignInbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignInActivity.class));
+            }
+        });
     }
 }
