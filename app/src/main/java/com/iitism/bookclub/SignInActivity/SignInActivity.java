76f4423795a056e91eb259851391f5cc79762722
rishabh-997 <rishabh.agarwal997@gmail.com>
2019-04-02 00:56:03 +0530

    Logging in and signing up using Id card Completedwith firebase auth

--- app/src/main/java/com/iitism/bookclub/SignInActivity/SignInActivity.java
+++ app/src/main/java/com/iitism/bookclub/SignInActivity/SignInActivity.java
@@ -2,6 +2,7 @@ package com.iitism.bookclub.SignInActivity;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +17,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.iitism.bookclub.R;
 import com.iitism.bookclub.SignUpActivity.SignUp_Page;
 
@@ -35,9 +38,16 @@ public class SignInActivity extends AppCompatActivity
                     "(?=\\S+$)" +           //no white spaces
                     ".{4,}" +               //at least 4 characters
                     "$");
+
 */
+
+    String details[]=new String[5];
+    int positions[]=new int[5];
+    int extras[]=new int[5];
+
+    IntentIntegrator intentIntegrator;
     private TextInputLayout email;
-    private TextInputLayout password;Button login;
+    private TextInputLayout password;Button login,scanner;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -47,7 +57,9 @@ public class SignInActivity extends AppCompatActivity
         setContentView(R.layout.activity_sign_in);
         email =  findViewById(R.id.login_email);
         login=findViewById(R.id.login);
+        scanner=findViewById(R.id.signin_scanner);
         password = findViewById(R.id.login_password);
+        intentIntegrator=new IntentIntegrator(this);
         email.setHint("Enter Email");
         password.setHint("Enter Password");
         TextView b1=findViewById(R.id.signin_signup);
@@ -66,6 +78,73 @@ public class SignInActivity extends AppCompatActivity
             }
         });
 
+        scanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intentIntegrator.initiateScan();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        IntentResult result=IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(result!=null)
+        {
+            if(result.getContents()==null)
+                Toast.makeText(this,"Nothing found",Toast.LENGTH_SHORT);
+            else
+            {
+                String s=result.getContents();
+                String name="Name";
+                String Admin="Admission No.";
+                String branch="Branch";
+                String email="E-mail";
+                String contact="Contact No.";
+                positions[0]=s.indexOf(name);
+                positions[1]=s.indexOf(Admin);
+                positions[2]=s.indexOf(branch);
+                positions[3]=s.indexOf(email);
+                positions[4]=s.indexOf(contact);
+                extras[0]=7;
+                extras[1]=15;
+                extras[2]=9;
+                extras[3]=8;
+                extras[4]=12;
+
+                for(int i=0;i<5;i++)
+                {
+                    String cut="";
+                    for(int j=positions[i];j>0;j++)
+                    {
+                        char ch=s.charAt(j);
+                        if (ch=='\n')
+                        {
+                            cut=s.substring(positions[i]+extras[i],j);
+                            break;
+                        }
+                    }
+                    details[i]=cut;
+                }
+
+                firebaseAuth.signInWithEmailAndPassword(details[3].trim(),details[1].trim())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful())
+                                {
+                                    Toast.makeText(SignInActivity.this,"Logged In",Toast.LENGTH_SHORT).show();
+                                }
+                                else
+                                    Toast.makeText(SignInActivity.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        }
+        else
+            super.onActivityResult(requestCode, resultCode, data);
 
     }
 
